cc4395f1fddce27373dcbd58d58a5e0236caad87
yoanr <yoanrktmg@gmail.com>
2024-07-10 23:01:14 +0300

    Fix headings

--- script.js
+++ script.js
@@ -86,10 +86,14 @@ function displayHeadingText(angle, midX, midY) {
 
     // Calculate the correct heading based on the mouse movement
     let displayedAngle;
-    if (angle >= 0 && angle < 180) {
-        displayedAngle = 180 - angle;
-    } else if (angle >= 180 && angle < 360) {
-        displayedAngle = 540 - angle;
+    if (angle >= 0 && angle < 90) {
+        displayedAngle = 90 - angle;
+    } else if (angle >= 90 && angle < 180) {
+        displayedAngle = 450 - angle; // 360 + 90 - angle
+    } else if (angle >= 180 && angle < 270) {
+        displayedAngle = 270 - (angle - 180);
+    } else if (angle >= 270 && angle < 360) {
+        displayedAngle = 90 - (angle - 270);
     } else if (angle === 360 || angle === 0) {
         displayedAngle = 0; // Special case for 360 or 0 degrees
     }
